01237456e0942aed3419d3fea1170f2e8c9a9300
Marcella Amorim <devabefarma4@outlook.com>
2025-11-06 12:34:08 -0300

    doc:  documentar api de cadastro de usuarios

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -25,49 +25,59 @@ namespace CafezinhoELivrosApi.Controllers
         ///<remarks>
         /// Cria um usuário com a role padrão de Leitor.
         ///</remarks>
+        /// <param name="user">Dados do usuário a ser criado.</param>
+        /// <returns>Retorna o usuário criado.</returns>
         [HttpPost]
+        [ProducesResponseType(typeof(UserResponseDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Index([FromBody] CreateUserDTO user)
         {
-            if (user == null)
-                return BadRequest("Usuário informado inválido");
-
-            if (await _dbContext.Users.AnyAsync(x => x.UserName == user.UserName || x.Email == user.Email))
-                return BadRequest("Já existe uma conta com esses nome de usuário/email");
-
-
-            var newUser = new User
+            try
             {
-                Name = user.Name,
-                UserName = user.UserName,
-                Email = user.Email,
-                BirthDate = user.BirthDate,
-                CurrentThought = user.CurrentThought,
-                City = user.City,
-                State = user.State,
-                Description = user.Description,
-                RoleId = await _roleManager.Roles.Where(r => r.Name == ERoles.Leitor.ToString()).Select(r => r.Id).FirstOrDefaultAsync()
-            };
+                if (user == null)
+                    return BadRequest("Usuário informado inválido");
 
-            var result = await _dbContext.AddAsync(newUser);
-            await _dbContext.SaveChangesAsync();
+                if (await _dbContext.Users.AnyAsync(x => x.UserName == user.UserName || x.Email == user.Email))
+                    return BadRequest("Já existe uma conta com esses nome de usuário/email");
 
-            // Ver AutoMapper
-            var response = new UserResponseDTO
+                var newUser = new User
+                {
+                    Name = user.Name,
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    BirthDate = user.BirthDate,
+                    CurrentThought = user.CurrentThought,
+                    City = user.City,
+                    State = user.State,
+                    Description = user.Description,
+                    RoleId = await _roleManager.Roles.Where(r => r.Name == ERoles.Leitor.ToString()).Select(r => r.Id).FirstOrDefaultAsync()
+                };
+
+                var result = await _dbContext.AddAsync(newUser);
+                await _dbContext.SaveChangesAsync();
+
+                // Ver AutoMapper
+                var response = new UserResponseDTO
+                {
+                    Id = new Guid(newUser.Id),
+                    Name = newUser.Name,
+                    UserName = newUser.UserName,
+                    Email = newUser.Email,
+                    BirthDate = newUser.BirthDate,
+                    CreatedAt = newUser.CreatedAt,
+                    CurrentThought = newUser.CurrentThought,
+                    Description = newUser.Description,
+                    City = newUser.City,
+                    State = newUser.State,
+                    Role = newUser.Role,
+                }; 
+
+                return Ok(response);
+            }catch(Exception ex)
             {
-                Id = new Guid(newUser.Id),
-                Name = newUser.Name,
-                UserName = newUser.UserName,
-                Email = newUser.Email,
-                BirthDate = newUser.BirthDate,
-                CreatedAt = newUser.CreatedAt,
-                CurrentThought = newUser.CurrentThought,
-                Description = newUser.Description,
-                City = newUser.City,
-                State = newUser.State,
-                Role = newUser.Role,
-            }; 
-
-            return Ok(response);
+                return StatusCode(500, ex.Message);
+            }
         }
 
 
